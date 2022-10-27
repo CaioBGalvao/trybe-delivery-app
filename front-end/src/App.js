001208dd2697d237bb8ae80faa99c1f02da013b0
Gabriel Coelho <coelho.tibbers@gmail.com>
2022-10-27 17:11:56 -0300

    Configuração inicial setada

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/login';
+import Register from './pages/register';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Routes>
+      <Route exact path="/" element={ <Login /> } />
+      <Route path="/login" element={ <Login /> } />
+      <Route path="/register" element={ <Register /> } />
+    </Routes>
   );
 }
 
---
+++ front-end/src/pages/register/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Register() {
+  return (<p>Tela de registro</p>);
+}
+
+export default Register;
