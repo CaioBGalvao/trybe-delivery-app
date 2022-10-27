e66df6b407d9aea3b77b499e84be4d8824e9b210
Gabriel Coelho <coelho.tibbers@gmail.com>
2022-10-27 17:56:43 -0300

    Componentes de login feitos

--- front-end/src/pages/login/index.jsx
+++ front-end/src/pages/login/index.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
 function Login() {
-  return (<p>Tela de login</p>);
+  return (
+    <>
+      <label htmlFor="emailLogin">
+        Login
+        <input id="emailLogin" type="email" data-testid="common_login__input-email" />
+      </label>
+      <label htmlFor="passwordLogin">
+        Senha
+        <input
+          id="passwordLogin"
+          type="password"
+          data-testid="common_login__input-password"
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="common_login__button-login"
+      >
+        LOGIN
+      </button>
+      <button
+        type="button"
+        data-testid="common_login__button-register"
+      >
+        Ainda não tenho conta
+      </button>
+    </>
+  );
 }
 
 export default Login;
