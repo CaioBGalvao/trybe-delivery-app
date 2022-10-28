681aac0bca7de8969c23ebd70aec33b6503457f1
luvlore <brunaluchini22@gmail.com>
2022-10-28 18:32:10 -0300

    feat: add user seeder

--- back-end/src/database/seeders/20221028212338-users.js
+++ back-end/src/database/seeders/20221028212338-users.js
@@ -1,24 +1,32 @@
 module.exports = {
   up: async (queryInterface, _Sequelize) => {
-    await queryInterface.bulkInsert('Users',
-      [{
-        id: 1,
-        displayName: 'Lewis Hamilton',
-        email: 'lewishamilton@gmail.com',
-        password: '123456',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg',
-      },
-      {
-        id: 2,
-        displayName: 'Michael Schumacher',
-        email: 'MichaelSchumacher@gmail.com',
-        password: '123456',
-        image: 'https://sportbuzz.uol.com.br/media/_versions/gettyimages-52491565_widelg.jpg',
-      },
+    await queryInterface.bulkInsert('users',
+      [
+        {
+          id: 1,
+          name: 'Delivery App Admin',
+          email: 'adm@deliveryapp.com',
+          password: 'a4c86edecc5aee06eff8fdeda69e0d04',
+          role: 'administrator', // senha: md5('--adm2@21!!--')
+        },
+        {
+          id: 2,
+          name: 'Fulana Pereira',
+          email: 'fulana@deliveryapp.com',
+          password: '3c28d2b0881bf46457a853e0b07531c6', // senha: md5('fulana@123')
+          role: 'seller',
+        },
+        {
+          id: 3,
+          name: 'Cliente Zé Birita',
+          email: 'zebirita@email.com',
+          password: '1c37466c159755ce1fa181bd247cb925', // senha: md5('$#zebirita#$')
+          role: 'customer',
+        },
       ], { timestamps: false });
   },
 
   down: async (queryInterface, _Sequelize) => {
-    await queryInterface.bulkDelete('Users', null, {});
+    await queryInterface.bulkDelete('users', null, {});
   },
 };
