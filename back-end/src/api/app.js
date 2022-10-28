5f2ed569d4db50826bb127e48f0c5090918d7400
luvlore <brunaluchini22@gmail.com>
2022-10-28 18:15:56 -0300

    feat: create product router

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('express-async-errors');
 const { ErrorHandler } = require('./middlewares');
-const routers = require('./routers');
+const router = require('./routers');
 
 const app = express();
 app.use(express.json());
@@ -9,7 +9,7 @@ app.use(express.json());
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 // rotas aqui
-app.use('/login', routers.loginRouter);
+app.use(router);
 
 app.use(ErrorHandler());
 module.exports = app;
