0e1a6199d0a443e3773d10aeb0c780b7d8757239
Caio Galvão <caio_bgalvao@hotmail.com>
2022-11-01 00:43:25 -0300

    Feat: Adicionado caso de erro para respostas null do banco de dados

--- back-end/src/api/services/product.services.js
+++ back-end/src/api/services/product.services.js
@@ -2,6 +2,11 @@ const { product } = require('../../database/models');
 
 const getAll = async () => {
   const products = await product.findAll();
+
+  if (!products) {
+    throw new Error('Could not find any products&404');
+  }
+
   return products;
 };
 
