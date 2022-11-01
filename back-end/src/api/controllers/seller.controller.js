5df1fc360eb3dddf377e057f9a875cf39cb88a00
Caio Galvão <caio_bgalvao@hotmail.com>
2022-11-01 19:28:56 -0300

    Fix: Retorno no controller
    Msg genérica substituída para resposta da API

--- back-end/src/api/controllers/seller.controller.js
+++ back-end/src/api/controllers/seller.controller.js
@@ -2,7 +2,7 @@ const { sellerServices } = require('../services');
 
 const getAllSellerUsers = async (req, res) => {
   const sellerUsers = await sellerServices.findAll();
-  return res.status(200).json({  message: 'all seller users listed' });
+  return res.status(200).json(sellerUsers);
 };
 
 module.exports = { getAllSellerUsers };
