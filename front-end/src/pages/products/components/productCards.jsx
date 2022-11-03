02826a81ec41c8cc071076b4dc0e403aee466f1b
Gabriel Coelho <coelho.tibbers@gmail.com>
2022-11-03 14:47:14 -0300

    Requisito 16, 14

--- front-end/src/pages/products/components/productCards.jsx
+++ front-end/src/pages/products/components/productCards.jsx
@@ -1,24 +1,86 @@
 import React, { useState, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
 import handleFetch from '../../../services/api';
+import { setIntoLocalStorage } from '../../../utils/localStorage';
 
 function RenderProducts() {
   // Configuracao inicial do state
 
   const [producsArray, setProductsArray] = useState([]);
+  const [quantity, setQuantity] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [checkoutStatus, setCheckoutStatus] = useState(false);
 
   useEffect(() => {
     const getProducts = async () => {
       const response = await handleFetch('GET', '/products');
+
       setProductsArray(response);
+
+      const formateQuantity = response.map(() => 0);
+
+      setQuantity(formateQuantity);
     };
     getProducts();
-  });
+  }, []);
+
+  const defineQuantity = (type, index, value) => {
+    if (type === 'add') {
+      const quantityResult = quantity.map((q, i) => {
+        if (i === index) return q + 1;
+        return q;
+      });
+
+      return setQuantity(quantityResult);
+    }
+
+    if (type === 'subtract') {
+      const quantityResult = quantity.map((q, i) => {
+        if (i === index && q !== 0) return q - 1;
+        return q;
+      });
+
+      return setQuantity(quantityResult);
+    }
+
+    if (type === 'input') {
+      const quantityResult = setQuantity(quantity[index] + Number(value));
+
+      return setQuantity(quantityResult);
+    }
+  };
+
+  useEffect(() => {
+    if (quantity && producsArray) {
+      const resultStorage = producsArray.map((product, index) => {
+        if (quantity[index] > 0) {
+          return {
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            qty: quantity[index],
+          };
+        }
+        return 'vazio';
+      });
+      const removeStrings = resultStorage.filter((element) => element !== 'vazio');
+      setIntoLocalStorage('carrinho', removeStrings);
+
+      let total = 0;
+      const formatePrices = producsArray.map((product, index) => (
+        (Number(product.price) * quantity[index]).toFixed(2)
+      ));
+      formatePrices.forEach((price) => {
+        total += Number(price);
+      });
+      setTotalPrice(total.toFixed(2));
+    }
+  }, [quantity, producsArray]);
 
   return (
     <div>
       {producsArray.map(
-        (product) => (
+        (product, index) => (
           <div
             data-testid={ `customer_products__element-card-price-${product.id}` }
             key={ product.id }
@@ -41,32 +103,39 @@ function RenderProducts() {
             <button
               type="button"
               data-testid={ `customer_products__button-card-rm-item-${product.id}` }
-              onClick={ totalPrice <= 0 ? '' : () => {
-                setTotalPrice(totalPrice - Number(product.price));
-              } }
+              onClick={ () => { defineQuantity('subtract', index); } }
             >
               -
             </button>
             <input
               type="number"
               data-testid={ `customer_products__input-card-quantity-${product.id}` }
-              value={ totalPrice }
-              defaultValue={ 0 }
-              min={ 0 }
+              onChange={
+                ({ target: { value } }) => defineQuantity('input', index, value)
+              }
+              value={ quantity[index] }
             />
             <button
               data-testid={ `customer_products__button-card-add-item-${product.id}` }
               type="button"
-              onClick={ () => { setTotalPrice(totalPrice + Number(product.price)); } }
+              onClick={ () => { defineQuantity('add', index); } }
             >
               +
             </button>
           </div>
         ),
       )}
-      <button type="button" data-testid="customer_products__checkout-bottom-value">
-        { `Ver Carrinho: R$${totalPrice}` }
+      <button
+        type="button"
+        data-testid="customer_products__button-cart"
+        onClick={ () => setCheckoutStatus(true) }
+        disabled={ totalPrice === '0.00' }
+      >
+        <p data-testid="customer_products__checkout-bottom-value">
+          { totalPrice !== 0 ? totalPrice.replace('.', ',') : 0 }
+        </p>
       </button>
+      { checkoutStatus === true ? <Navigate to="/customer/checkout" /> : '' }
     </div>
   );
 }
