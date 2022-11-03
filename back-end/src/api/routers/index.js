3b72b647ace0117cb80df69280ee469866faa3a1
Caio Galvão <caio_bgalvao@hotmail.com>
2022-11-03 16:03:29 -0300

    Sequilize create feito

--- back-end/src/api/routers/index.js
+++ back-end/src/api/routers/index.js
@@ -1,6 +1,6 @@
 const loginRouter = require('./login.routes');
 const productRouter = require('./product.routes');
-const sellerRouter = require('./seller.routes')
+const sellerRouter = require('./seller.routes');
 const checkoutRouter = require('./checkout.routes');
 const salesRouter = require('./sales.routes');
 
--- back-end/src/api/services/checkout.services.js
+++ back-end/src/api/services/checkout.services.js
@@ -1,1 +1,33 @@
 const { sale, salesProduct } = require('../../database/models');
+
+const newSales = (objSales) => {
+  const verifiedSales = validateSales(objSales);
+
+  const createSalesResponse = sale.create({
+    userId: verifiedSales.userId,
+    sellerId: verifiedSales.sellerId,
+    deliveryAddress: verifiedSales.deliveryAddress,
+    deliveryNumber: verifiedSales.deliveryNumber,
+    totalPrice: verifiedSales.totalPrice,
+    saleDate: new Date(),
+  });
+
+};
+
+// "userId": "1",
+//  "sellerId": "1",
+//  "salesProducts": [
+//    {
+//      "productId": "1",
+//      "quantity": "1"
+//    },
+//    {
+//      "productId": "2",
+//      "quantity": "2"
+//    }
+//  ],
+//  "deliveryAddress": "Rua do Bobo",
+//  "deliveryNumber": "0",
+//  "totalPrice": '100.24',
+//  "saleDate": "22-07-2022"
+// }
