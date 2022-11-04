11e9c38295c5592fbcb15ccbc1a232533137049d
Caio Galvão <caio_bgalvao@hotmail.com>
2022-11-04 14:50:35 -0300

    Fix: Encapsulamento de funções não funciona para middleware do express

--- back-end/src/api/controllers/product.controller.js
+++ back-end/src/api/controllers/product.controller.js
@@ -1,6 +1,7 @@
 const { productServices } = require('../services');
 
 const getAll = async (_req, res) => {
+
   const products = await productServices.getAll();
   return res.status(200).json(products);
 };
--- back-end/src/api/routers/seller.routes.js
+++ back-end/src/api/routers/seller.routes.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
 const { sellerController } = require('../controllers');
-const { validateTokenParams } = require('../security/jwt');
+const { validateToken } = require('../security/jwt');
 
 const sellerRouter = Router();
 
 sellerRouter
-  .get('/', validateTokenParams, sellerController.getAllSellerUsers);
+  .get('/', validateToken, sellerController.getAllSellerUsers);
 
 module.exports = sellerRouter;
