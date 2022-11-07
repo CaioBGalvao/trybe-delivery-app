42f4fc44b32479557c0f5190f441b511cec134f0
Caio Galvão <caio_bgalvao@hotmail.com>
2022-11-07 19:18:46 -0300

    Fix: Erro de duplo response corrigido no controller

--- back-end/src/api/controllers/checkout.controller.js
+++ back-end/src/api/controllers/checkout.controller.js
@@ -12,30 +12,32 @@ const checkout = async (req, res) => {
   res.status(201).json(response);
 };
 
-const sellerStatus = async (req, res, sellerId) => {
+const sellerStatus = async (req, sellerId) => {
   const { id: saleId } = req.params;
   const { status } = req.body;
   await securityServices.saleOwnerVerify({ role: 'seller', id: sellerId, saleId });
   const response = await checkoutServices.sellerCheckout(saleId, status);
-  res.status(200).json({ message: response });
+  return response;
 };
-
-const customerStatus = async (req, res, userId) => {
+// Function Comum
+const customerStatus = async (req, userId) => {
   const { id: saleId } = req.params;
   const { status } = req.body;
   await securityServices.saleOwnerVerify({ role: 'customer', id: userId, saleId });
   const response = await checkoutServices.customerCheckout(saleId, status);
-  res.status(200).json({ message: response });
+  return response;
 };
-
+// middleware Express
 const decisionMaker = async (req, res) => {
   const { email } = req.body;
   const { role, id: userId } = await securityServices.roleVerify(email);
   if (role === 'seller') {
-    await sellerStatus(req, res, userId);
+    const response = await sellerStatus(req, userId);
+    return res.status(200).json({ message: response });
   }
   if (role === 'customer') {
-    await customerStatus(req, res, userId);
+    const response = await customerStatus(req, userId);
+    return res.status(200).json({ message: response });
   }
   throw new Error('Your role does not allow you to access this endpoint&401');
 };
